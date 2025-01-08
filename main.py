a7ba00129a297c5d9eb6f194e67e0c03c777a4f6
Stas <stasnovalenco3@gmail.com>
2025-01-08 21:25:29 +0200

    Finished the project

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ def main():
     while True:
         print("1. Log in")
         print("2. Sign in")
-        print("3. Display accounts")
         print("0. Exit")
         
         option= int(input("Introduce your option: "))
@@ -13,8 +12,6 @@ def main():
                 log_in()
             case 2:
                 sign_in()
-            case 3:
-                display_acc()
             case 0:
                 break
 
@@ -45,10 +42,16 @@ def log_in():
     username_input= input("Introduce your username: ")
     password_input= input("Introduce your password: ")
 
+    logged= False
     acc_json= import_acc()
     for i in range(len(acc_json)):
-        print(f"username: {acc_json[i]["username"]}")
-        print(f"password: {acc_json[i]["password"]}")
+        if(acc_json[i]["username"] == username_input and acc_json[i]["password"] == password_input):
+            logged= True
+
+    if logged:
+        print("You logged in successfully!")
+    else:
+        print("The data introduced does not match or the account does not exist")
 
 def sign_in():
     acc_json= import_acc()
@@ -68,11 +71,6 @@ def sign_in():
         acc_json.pop()
 
 
-def display_acc():
-    with open("accounts.json", "r") as json_file:
-        accounts= json.load(json_file)
-
-    print(accounts)
 
 
 main()
